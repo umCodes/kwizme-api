14338789a784018d0c6121a93cfdfae9259c1b5c
umCodes <umohammed.official@gmail.com>
2025-06-16 15:42:52 +0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import { quizRoutes } from './routes/quizRoutes';
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'http://localhost:5173/', credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 
